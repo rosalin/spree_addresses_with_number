7fe1a2ec3983157d77293d955c1bba77677beaf3
rosalin <rodrigorosalin2@yahoo.com.br>
2013-08-27 11:21:49 -0300

    patch address

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -2,11 +2,23 @@ module Spree
   Order.class_eval do
   
    Spree::Order.state_machine.before_transition :to => :delivery, :do => :concat
+   
+   Spree::Order.state_machine.before_transition :to => :address, :do => :normalize_address 
     
     
     def concat
       self.bill_address.address1 = self.bill_address.address1 + ", " + self.bill_address.number
       #self.bill_address.address1 = self.bill_address.address1 + ", " + self.ship_address.number
+    end
+    
+    def normalize_address
+      virgula_index=nil
+      virgula_index = self.bill_address.address1.index(',')  
+      
+      if(virgula_index != nil)
+        self.bill_address.address1= self.bill_address.address1[0, virgula_index]
+      end
+      
     end
 
   end
